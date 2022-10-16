38c7eb4ab92f4d2a190ce9eaf9c23a5eaba41900
gtr-hub <109278869+gtr-hub@users.noreply.github.com>
2022-10-16 21:40:34 +0530

    Update Sms.java

--- Sms.java
+++ Sms.java
@@ -1,10 +1,20 @@
-import java.time.format.DateTimeFormatter;
-import java.time.LocalDateTime;  
-public class Sms {  
-  public static void main(String[] args) {  
-   DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-   LocalDateTime now = LocalDateTime.now();
- 
-   System.out.println(dtf.format(now));
-  }  
+import java.applet.Applet;  
+import java.awt.*;  
+  
+public class GraphicsDemo extends Applet{  
+  
+public void paint(Graphics g){  
+g.setColor(Color.red);  
+g.drawString("Welcome",50, 50);  
+g.drawLine(20,30,20,300);  
+g.drawRect(70,100,30,30);  
+g.fillRect(170,100,30,30);  
+g.drawOval(70,200,30,30);  
+  
+g.setColor(Color.pink);  
+g.fillOval(170,200,30,30);  
+g.drawArc(90,150,30,30,30,270);  
+g.fillArc(270,150,30,30,0,180);  
+  
+}  
 }  
